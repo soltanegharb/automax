825dbf9f93f3859ed4ffb3fe6e619ee4b859ed3c
Mohammad Hossein Soltani <soltanisoltanegharb@gmail.com>
2025-04-26 18:14:00 +0330

    Created templates directory in main app
    initialized the template and added bootstrap to it.

--- main/views.py
+++ main/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def main_view(request):
-    return HttpResponse("<h1>Welcome to Automax!</h1>")
+    return render(request, 'views/main.html', {"name": "Automax"})
