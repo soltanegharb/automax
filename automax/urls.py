d4657480fdec728b536f4562e31218800976d402
Mohammad Hossein Soltani <soltanisoltanegharb@gmail.com>
2025-04-26 17:25:16 +0330

    Writing a 'welcom to automax' just for testing

--- automax/urls.py
+++ automax/urls.py
@@ -16,7 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls import include
+
+from main import urls as main_app_urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include(main_app_urls))
 ]
